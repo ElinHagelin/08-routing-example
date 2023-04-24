6b9b48e9b7435e4be6c443dc7c2d210855eabd4b
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-04-24 16:05:47 +0200

    Added some css and a grid to products-page

--- src/routeConfig.jsx
+++ src/routeConfig.jsx
@@ -1,10 +1,11 @@
 import { createHashRouter } from 'react-router-dom'
 import Root from './routes/Root.jsx'
 import Start from './routes/Start.jsx'
-import Products from './routes/Products.jsx'
+import Products, { loader as productsLoader } from './routes/Products.jsx'
 import './index.css'
 import About from './routes/About.jsx'
 import ErrorPage from './routes/ErrorPage.jsx'
+import ProductDetails from './routes/ProductDetails.jsx'
 
 // Skapa en router med konfiguration, som beskriver all routes som ska finas in appen.
 const router = createHashRouter([
@@ -21,11 +22,23 @@ const router = createHashRouter([
 			},
 			{
 				path: 'products',
-				element: <Products />
+				element: <Products />,
+				loader: productsLoader,
+				// children: [
+				// 	{
+				// 		path: 'details',
+				// 		element: <ProductDetails />
+				// 	}
+				// ]
 			},
 			{
 				path: 'about',
 				element: <About />
+			},
+			{
+				path: 'details/:id',
+				element: <ProductDetails />,
+				loader: productsLoader
 			}
 		],
 		// Används om URL inte matchar någon tidigare
--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,8 +1,36 @@
-const Products = () =>
-(
-	<section>
-		Products
-	</section>
-)
+import { NavLink, Outlet, useLoaderData, Link } from "react-router-dom"
+import ProductDetails from "./ProductDetails"
+
+const testData = [
+	{ "id": 1, "name": "Sewing Machine", "short_description": "A mechanical or electronic device that sews fabrics together with thread.", "price": 299.99 },
+	{ "id": 2, "name": "Seam Ripper", "short_description": "A small tool used for cutting and removing stitches.", "price": 3.99 },
+	{ "id": 3, "name": "Scissors", "short_description": "A cutting instrument consisting of two blades with finger holes.", "price": 12.99 },
+	{ "id": 4, "name": "Measuring Tape", "short_description": "A flexible ruler used to take body measurements and measure fabrics.", "price": 2.99 },
+	{ "id": 5, "name": "Pins", "short_description": "Thin metal or plastic rods with pointed ends used to hold fabrics together.", "price": 1.99 },
+	{ "id": 6, "name": "Thread", "short_description": "A long, thin strand of cotton or other material used for sewing.", "price": 4.99 },
+	{ "id": 7, "name": "Iron", "short_description": "A device used for pressing wrinkles out of fabrics.", "price": 39.99 },
+	{ "id": 8, "name": "Rotary Cutter", "short_description": "A cutting tool with a circular blade used for cutting fabrics.", "price": 24.99 }
+]
+
+export const loader = () => testData
+
+
+const Products = () => {
+	const productData = useLoaderData()
+	return (
+		<section>
+			<h1 className="products-heading"> Products </h1>
+			<ul className="product-list">
+				{productData.map(({ id, name, price, short_description }) => (
+					<li key={id} className="product-card">
+						<h3><Link to={'/details/' + id}>{name}</Link></h3>
+						<p>{price}</p>
+						<p>{short_description}</p>
+					</li>
+				))}
+			</ul>
+		</section>
+	)
+}
 
 export default Products
